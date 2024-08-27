83975bbb6b28ad1a8ab3f1187a22c2f13417acd1
jose <josesoriap21@gmail.com>
2024-08-27 01:41:52 -0300

    navbar

--- src/pages/Ecommerce/index.tsx
+++ src/pages/Ecommerce/index.tsx
@@ -1,5 +1,6 @@
 import Categories from "../../components/Categories";
 import Footer from "../../components/Footer";
+import Herosection from "../../components/Hero/Herosection";
 import Navbar from "../../components/Navbar";
 import Product from "../../components/Product";
 import Whatsapp from "../../components/Whatsapp";
