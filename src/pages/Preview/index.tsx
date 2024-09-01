c7d027c33dfd3060af7d8c5fda30aed67d7ec1dc
Jose <josesoriap21@gmail.com>
2024-08-31 21:43:41 -0300

    slider

--- src/pages/Preview/index.tsx
+++ src/pages/Preview/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import Ecommerce from '../Ecommerce'
+import React from "react";
+import Ecommerce from "../Ecommerce";
 
 const index = () => {
-    return (
-        <>
-            <div className='w-full flex flex-col gap-5 items-center p-10'>
-                <h4 className='text-[30px] font-semibold text-green-500'>Visualización de pagina</h4>
-                <div className='w-[600px] h-screen shadow-2xl relative overflow-y-auto overflow-x-hidden resize'>
-                    <Ecommerce />
-                </div>
-            </div>
-        </>
-    )
-}
+  const unit: Unit = {
+    id: "example-id",
+    name: "Example Unit",
+    // Agrega otros campos necesarios de acuerdo al tipo Unit
+  };
 
-export default index
+  return (
+    <>
+      <div className="w-full flex flex-col gap-5 items-center p-10">
+        <h4 className="text-[30px] font-semibold text-green-500">
+          Visualización de pagina
+        </h4>
+        <div className="w-[600px] h-screen shadow-2xl relative overflow-y-auto overflow-x-hidden resize">
+          <Ecommerce unit={unit} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default index;
