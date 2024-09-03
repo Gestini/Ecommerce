dd0c410c2f49a6bd33b67ee7b3f16ce80a95be73
jose <josesoriap21@gmail.com>
2024-09-03 01:11:54 -0300

    navbar

--- src/components/Landing/Navbar.tsx
+++ src/components/Landing/Navbar.tsx
@@ -5,38 +5,40 @@ import { GestinyLogo } from "../../assets/Logo";
 
 const Navbar = () => {
   return (
-    <div className=" navbar--landing z-10 flex w-[100%] gap-4 items-center absolute  px-[100px] pt-[15px] ">
-      <div className="izq-navbar--landing flex ">
-        <div className="logo--landing  ">
-          {" "}
-          <GestinyLogo />
+    <div className=" navbar--landing z-10 flex w-[100%] justify-center mx-auto gap-4 items-center absolute   pt-[15px] ">
+      <div className="navbarmedio flex w-[80%]  ">
+        <div className="izq-navbar--landing flex ">
+          <div className="logo--landing  ">
+            {" "}
+            <GestinyLogo />
+          </div>
         </div>
-      </div>
-      <div className="items-navbar--landing flex flex-grow px-[50px] gap-4 ">
-        <div className="item-navbar--landing cursor-pointer  gap-2 flex justify-center items-center text-[#9b9b9b]">
-          <span className=" font-[500] ">Soluciones</span>
-          <IoIosArrowDown className="  " />
+        <div className="items-navbar--landing flex flex-grow px-[50px] gap-4 ">
+          <div className="item-navbar--landing cursor-pointer  gap-2 flex justify-center items-center text-[#9b9b9b]">
+            <span className=" font-[500] ">Soluciones</span>
+            <IoIosArrowDown className="  " />
+          </div>
+          <div className="item-navbar--landing cursor-pointer   gap-2 flex justify-center items-center text-[#9b9b9b]">
+            <span className=" font-[500] ">Descargar</span>
+          </div>
+          <div className="item-navbar--landing cursor-pointer  gap-2 flex justify-center items-center text-[#9b9b9b]">
+            <span className=" font-[500] ">Equipo</span>
+          </div>
+          <div className="item-navbar--landing cursor-pointer  gap-2 flex justify-center items-center text-[#9b9b9b]">
+            <span className=" font-[500] ">Centro de ayuda</span>
+          </div>
+          <div className="item-navbar--landing cursor-pointer  gap-2 flex justify-center items-center text-[#9b9b9b]">
+            <span className=" font-[500] ">Precios</span>
+          </div>
         </div>
-        <div className="item-navbar--landing cursor-pointer   gap-2 flex justify-center items-center text-[#9b9b9b]">
-          <span className=" font-[500] ">Descargar</span>
+        <div className="der-navbar--landing w-[300px] flex items-center justify-between  h-full ">
+          <div className="item-navbar--landing  gap-2 flex justify-center items-center text-[#9b9b9b]">
+            <span className=" font-[500] ">Iniciar Sesion </span>
+          </div>
+          <button className="  bg-[#A0DB8E] px-[20px] py-[10px] rounded-lg font-[600] ">
+            Empieza gratis
+          </button>
         </div>
-        <div className="item-navbar--landing cursor-pointer  gap-2 flex justify-center items-center text-[#9b9b9b]">
-          <span className=" font-[500] ">Equipo</span>
-        </div>
-        <div className="item-navbar--landing cursor-pointer  gap-2 flex justify-center items-center text-[#9b9b9b]">
-          <span className=" font-[500] ">Centro de ayuda</span>
-        </div>
-        <div className="item-navbar--landing cursor-pointer  gap-2 flex justify-center items-center text-[#9b9b9b]">
-          <span className=" font-[500] ">Precios</span>
-        </div>
-      </div>
-      <div className="der-navbar--landing w-[300px] flex items-center justify-between  h-full ">
-        <div className="item-navbar--landing  gap-2 flex justify-center items-center text-[#9b9b9b]">
-          <span className=" font-[500] ">Iniciar Sesion </span>
-        </div>
-        <button className="  bg-[#A0DB8E] px-[20px] py-[10px] rounded-lg font-[600] ">
-          Empieza gratis
-        </button>
       </div>
     </div>
   );
